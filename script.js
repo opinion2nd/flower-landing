17179cb38d68d870bd5faffc958c573d434fc802
opinion2nd <hakaistan@gmail.com>
2026-01-15 14:06:38 +0600

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function dropPetals(flower) {
-  const petals = flower.querySelectorAll('.petal');
-  petals.forEach((petal, i) => {
-    setTimeout(() => {
-      petal.classList.add('fall');
-    }, i * 150);
-  });
-}
